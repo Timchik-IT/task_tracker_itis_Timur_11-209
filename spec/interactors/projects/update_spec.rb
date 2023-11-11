065279c6b580ff52104929bb52ee8ba7b1e7b08e
Timur <timur.minullin535@gmail.com>
2023-11-11 22:18:20 +0300

    add tests on project updating

---
+++ spec/interactors/projects/update_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+
+describe Projects::Update do
+  describe ".call" do
+    let(:project) { create :project }
+    let(:interactor) { described_class.new(project: project, project_params: params) }
+
+    context "when params are valid" do
+      let(:params) do
+        {
+          name: "new name",
+          description: "new description"
+        }
+      end
+
+      it "project successfuly update" do
+        interactor.run
+
+        expect(project.name).to eq params[:name]
+        expect(project.description).to eq params[:description]
+      end
+    end
+
+    context "when params are invalid" do
+      let(:params) do
+        {
+          name: "",
+          description: "new description"
+        }
+      end
+
+      let(:expected_error_message) { "Invalid data" }
+
+      it "throws error" do
+        interactor.run
+
+        expect(interactor.context.error).to eq(expected_error_message)
+      end
+    end
+  end
+end
